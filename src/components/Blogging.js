8ca7ae66da748d0cb7cefa035ad48a77f315040a
Parada <paradafaii28@gmail.com>
2021-01-04 13:35:58 -0700

    add form component to blog page

--- src/components/Blogging.js
+++ src/components/Blogging.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
-export default class Blogging extends Component {
-    render() {
-        return (
-            <div>
-                Blogging
-            </div>
-        )
-    }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+      backgroundColor: "white"
+    },
+  },
+}));
+
+export default function MultilineTextFields() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('Controlled');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Multiline"
+          multiline
+          rowsMax={4}
+          value={value}
+          onChange={handleChange}
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-textarea"
+          label="Multiline Placeholder"
+          placeholder="Placeholder"
+          multiline
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+          variant="outlined"
+        />
+      </div>
+    </form>
+  );
 }
